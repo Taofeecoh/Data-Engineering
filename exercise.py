7658f9a2f40ec2dff1af21e5c0a5817e355eb953
Taofeecoh ADESANU <107361388+Taofeecoh@users.noreply.github.com>
2025-03-13 23:46:38 +0100

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -19,25 +19,10 @@ print("\n")
 print("Task 4:")
 entry = ["Wofai", "Na(me", "Zainab", "A4atullah", "Women42k", "Norm1al"]
 print(md.task_four(entry))
+print("\n")
 
-"""for i in data:
-    for j in i:
-        if not j.isalpha():
-            bad_data.append(i)
-print(f"Bad inputs in entry: {bad_data}")"""
-
-"""
-names[2] = names[2].replace(names[2][-1], "a") 
-print(names[2])
-
-if names[0][0].isupper():
-    print(names[0])
-else:
-    print("Not uppper")
-
-
-if names[0][0].isupper() and not names[0][-1].endswith("a"):
-    print("Yes!")
-else:
-    print("No!")
-"""
+print("Task 5:")
+cust_names = [10, "mayowa", "zainab", 4, 5]
+yielded = md.yield_bad_names(cust_names)
+for i in yielded:
+    print(i)
